e14d549aa4bad991fbd0bf987c2e8a4a73433765
Krystian Ostrowski <krystian.ostrowski@outlook.com>
2020-04-18 23:27:40 +0200

    AuthService added

---
+++ WitDrive/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WitDrive.Models;
+
+namespace WitDrive.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> GenerateJwtToken(User user, UserManager<User> userManager, IConfiguration config);
+    }
+}
---
+++ WitDrive/Services/AuthService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using WitDrive.Interfaces;
+using WitDrive.Models;
+
+namespace WitDrive.Services
+{
+    public class AuthService : IAuthService
+    {
+        public async Task<string> GenerateJwtToken(User user, UserManager<User> userManager, IConfiguration config)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var roles = await userManager.GetRolesAsync(user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.GetSection("AppSettings:Token").Value));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
