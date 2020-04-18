c36df7dbb7b385c9b5cc63dea552e2c354c8dd11
Krystian Ostrowski <krystian.ostrowski@outlook.com>
2020-04-18 22:50:27 +0200

    Mapping profile added

---
+++ WitDrive/Data/Configs/MappingProfileConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WitDrive.Dto;
+using WitDrive.Models;
+
+namespace WitDrive.Data.Configs
+{
+    public class MappingProfileConfiguration : Profile
+    {
+        public MappingProfileConfiguration()
+        {
+            CreateMap<User, UserForListDto>();
+            CreateMap<UserForRegisterDto, User>();
+        }
+    }
+}
