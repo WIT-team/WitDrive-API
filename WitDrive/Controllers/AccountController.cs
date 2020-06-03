3778fbf38a849e94e457623bff63062e0f6e7bcf
Krystian Ostrowski <krystian.ostrowski@outlook.com>
2020-06-03 12:49:13 +0200

    Password edit added

--- WitDrive/Controllers/AccountController.cs
+++ WitDrive/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using WitDrive.Dto;
 using WitDrive.Interfaces;
@@ -25,23 +26,23 @@ namespace WitDrive.Controllers
             this.userManager = userManager;
         }
 
-        [HttpPost("forgot-password")]
-        public async Task<IActionResult> ForgotPassword(ForgotPasswordDto forgotPasswordDto)
-        {
-            var user = await userManager.FindByEmailAsync(forgotPasswordDto.Email);
-            if (user == null)
-            {
-                return NotFound();
-            }
+        //[HttpPost("forgot-password")]
+        //public async Task<IActionResult> ForgotPassword(ForgotPasswordDto forgotPasswordDto)
+        //{
+        //    var user = await userManager.FindByEmailAsync(forgotPasswordDto.Email);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var token = await userManager.GeneratePasswordResetTokenAsync(user);
-            var callback = Url.RouteUrl("ResetPasswordModel", new { token, email = user.Email }, Request.Scheme);
+        //    var token = await userManager.GeneratePasswordResetTokenAsync(user);
+        //    var callback = Url.RouteUrl("ResetPasswordModel", new { token, email = user.Email }, Request.Scheme);
 
-            var message = new Message(new string[] { forgotPasswordDto.Email }, "Link to reset your password", callback);
-            await emailSender.SendEmailAsync(message);
+        //    var message = new Message(new string[] { forgotPasswordDto.Email }, "Link to reset your password", callback);
+        //    await emailSender.SendEmailAsync(message);
 
-            return Ok();
-        }
+        //    return Ok();
+        //}
 
         //[HttpPost]
         //[ValidateAntiForgeryToken]
@@ -63,29 +64,48 @@ namespace WitDrive.Controllers
         //    return Ok();
         //}
 
-        [HttpPost("reset-password")]
-        public async Task<IActionResult> ResetPasswordxD([FromQuery] ResetPasswordDto resetPasswordDto)
+        //[HttpPost("reset-password")]
+        //public async Task<IActionResult> ResetPassword([FromQuery] ResetPasswordDto resetPasswordDto)
+        //{
+        //    var user = await userManager.FindByEmailAsync(resetPasswordDto.Email);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var result = await userManager.ResetPasswordAsync(user, resetPasswordDto.Token, resetPasswordDto.Password);
+        //    if (!result.Succeeded)
+        //    {
+        //        return BadRequest(result.Errors);
+        //    }
+
+        //    return Ok();
+        //}
+
+        //[HttpGet("reset-password-model", Name = "ResetPasswordModel")]
+        //public IActionResult ResetPassword(string token, string email)
+        //{
+        //    var model = new ResetPasswordDto { Token = token, Email = email };
+        //    return Ok(model);
+        //}
+
+        [HttpPost("u/{userId}/edit-password")]
+        public async Task<IActionResult> ResetPassword(EditPasswordDto editPasswordDto, int userId)
         {
-            var user = await userManager.FindByEmailAsync(resetPasswordDto.Email);
-            if (user == null)
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
             {
-                return NotFound();
+                return Unauthorized();
             }
 
-            var result = await userManager.ResetPasswordAsync(user, resetPasswordDto.Token, resetPasswordDto.Password);
-            if (!result.Succeeded)
+            User appUser = await userManager.GetUserAsync(this.User);
+            var result = await userManager.ChangePasswordAsync(appUser, editPasswordDto.OldPassword, editPasswordDto.NewPassword);
+
+            if (result.Succeeded)
             {
-                return BadRequest(result.Errors);
+                return Ok();
             }
 
-            return Ok();
-        }
-
-        [HttpGet("reset-password-model", Name = "ResetPasswordModel")]
-        public IActionResult ResetPassword(string token, string email)
-        {
-            var model = new ResetPasswordDto { Token = token, Email = email };
-            return Ok(model);
+            return BadRequest(result.Errors);
         }
 
     }
